2e3cdb7eaecd7905aaf00fc6fad2815bdd74d394
Lazaro Altedill Hernandez <103474812+lazaroah02@users.noreply.github.com>
2022-08-29 14:22:04 -0400

    Delete NoteApp directory

--- NoteApp/apps/notes/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class add_note_form(forms.Form):
-    title = forms.CharField(max_length = 100, required = False)
-    note = forms.CharField(widget=forms.Textarea, required = False)
--- NoteApp/apps/notes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.notes import views
-urlpatterns = [
-    path("", views.notes),
-    path("api/", views.HelloApiView.as_view()),
-    path("api/<int:note_id>/", views.HelloApiView.as_view()),
-    path("authentication/login/", views.login)
-]
